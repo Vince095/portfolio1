0672eea0cedb71acad583cafc2d5d4febc6ebee4
Vince095 <45779146+Vince095@users.noreply.github.com>
2026-01-13 13:12:12 +0200

    Update experience from three to five years

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Vincent, a software engineer with over three years of experience.
+            Hi! I&apos;m Vincent, a software engineer with over five years of experience.
           </p>
 
           <a href="#about">
